2daa82141bb97e3609d188b0ae37350a65103f08
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-09-13 12:54:31 -0700

    adding angular todo

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../model/Todo';
+import {TodoService} from '../../services/todo.service';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+  constructor() { }
+
+  ngOnInit(): void {
+  
+  }
+
+}
